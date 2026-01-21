592abfe08b92890a17f20d6777d8b0f04cacdc1a
Ratul Sur <ratulsur@gmail.com>
2026-01-21 11:12:16 +0530

    update code

--- research_and_analyst/schemas/models.py
+++ research_and_analyst/schemas/models.py
@@ -1,1 +1,6 @@
 from langchain_core.messages import AIMessage, SystemMessage, HumanMessage
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+search = TavilySearchResults()
+
+search.invoke("who is Lionel Messi?")
