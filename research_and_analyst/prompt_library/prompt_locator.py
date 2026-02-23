bdd9f0f829fc32483080ea3b1c0e9ffd32953b04
Ratul Sur <ratulsur@gmail.com>
2026-02-23 23:50:04 +0530

    code updated

--- research_and_analyst/prompt_library/prompt_locator.py
+++ research_and_analyst/prompt_library/prompt_locator.py
@@ -2,3 +2,11 @@ from jinja2 import Environment, BaseLoader
 
 jinja_env = Environment(loader=BaseLoader())
 
+CREATE_ANALYSIS_PROMPT = jinja_env.from_string("""
+you are tasked with creating a set of AI analysts. please follow the instructions:
+1. first review the reseearch topic:
+                                                                                           
+                                                                                                                                         
+                                               
+                                                                                             
+""")                                                                                    )
