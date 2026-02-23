f75b72d02f323b56c50a53d9bac6e33ee0c8f47c
Ratul Sur <ratulsur@gmail.com>
2026-02-23 23:59:40 +0530

    code updated

--- research_and_analyst/prompt_library/prompt_locator.py
+++ research_and_analyst/prompt_library/prompt_locator.py
@@ -2,10 +2,26 @@ from jinja2 import Environment, BaseLoader
 
 jinja_env = Environment(loader=BaseLoader())
 
-CREATE_ANALYSIS_PROMPT = jinja_env.from_string("""
+CREATE_ANALYSIS_PROMPT = jinja_env.from_string
+("""
 you are tasked with creating a set of AI analysts. please follow the instructions:
 1. first review the reseearch topic:
-                                                                                           
+{% if topic %}
+{{ topic}}
+{% else %}
+[No topic provided, focus on a more generic topic relevant to AI research and analysis]
+{% endif %}
+
+2. Examine any editorial feedback that has been optionally provided to guide creation of the analysts:
+{% if human_analyst_feedback %}
+{{ human_analysts_feedback}}
+{ % else %}
+[No feedback given. Us eyour own discretion]
+{% endif %}
+
+3. 
+
+                                                                                                                                        
                                                                                                                                          
                                                
                                                                                              
