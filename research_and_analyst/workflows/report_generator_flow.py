6ae48cd4d34d46a5bb80bad8d0a44bff4618ae2d
Ratul Sur <ratulsur@gmail.com>
2026-02-26 12:05:28 +0530

    class initiated

--- research_and_analyst/workflows/report_generator_flow.py
+++ research_and_analyst/workflows/report_generator_flow.py
@@ -11,7 +11,7 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from docx import Document
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
-
+from langgraph.checkpoint.memory import MemorySaver
 from research_and_analyst.schemas.models import Perspectives, GenerateAnalystsState, ResearchGraphState
 from research_and_analyst.utils.model_loader import ModelLoader
 from research_and_analyst.workflows.interview_workflow import InterviewGraphBuilder
@@ -20,3 +20,17 @@ from research_and_analyst.prompt_library.prompt_locator import CREATE_ANALYSIS_P
 from research_and_analyst.log.logger import CustomLogger
 from research_and_analyst.exception.custom_exception import ResearchAnalystException
 
+
+
+class AutonomousReportGenerator:
+    """
+    handles end-to-end autonomous report generation
+    """
+    def __init__(self, llm):
+        self.llm = llm
+        self.memory = MemorySaver()
+        tavily_search = TavilySearchResults(tavily_api_key = os.getenv("TAVILY_API_KEY")
+                                            ) 
+        self.logger = CustomLogger.bind(module = "AutonomousReportGenerator")
+
+    
